eab19b3e09c646fa1af43c22ba8267e46f6e7d4e
Andrew Lundy <andrewlundy@icloud.com>
2023-01-20 08:06:14 -0600

    Successfully connected to database via Go

--- main.go
+++ main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"github.com/go-sql-driver/mysql"
 	"log"
 	"os"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
 func main() {
 	cfg := mysql.Config{
-		User:   os.Getenv("DBUSER"),
-		Passwd: os.Getenv("DBPASS"),
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "recordings",
+		User:                 os.Getenv("DBUSER"),
+		Passwd:               os.Getenv("DBPASS"),
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "recordings",
+		AllowNativePasswords: true,
 	}
 
 	var err error
