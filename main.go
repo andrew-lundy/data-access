13272e31a9ec16c2f2d344a94ea8c2b341305a47
Andrew Lundy <andrewlundy@icloud.com>
2023-01-21 19:01:00 -0600

    Finish calling single row

--- main.go
+++ main.go
@@ -45,6 +45,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	album, err := albumByID(3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v by %v for $%v\n", album.Title, album.Artist, album.Price)
 }
 
 func albumsByArtist(name string) ([]Album, error) {
@@ -68,4 +74,27 @@ func albumsByArtist(name string) ([]Album, error) {
 	}
 
 	return albums, nil
+}
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+
+	err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+
+	//if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+	//	if err == sql.ErrNoRows {
+	//		return alb, fmt.Errorf("albumsById %d: no such album", id)
+	//	}
+	//	return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	//}
+
+	return alb, nil
 }
