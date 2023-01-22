a95e0078a10baac1d2123fba7d3050433bddf688
Andrew Lundy <andrewlundy@icloud.com>
2023-01-22 12:56:36 -0600

    Finish 'Access a relational database'

--- main.go
+++ main.go
@@ -51,6 +51,16 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Album found: %v by %v for $%v\n", album.Title, album.Artist, album.Price)
+
+	albID, err := addAlbum(Album{
+		Title:  "Smokin' at the Half Note",
+		Artist: "Wes Montgomery",
+		Price:  31.52,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albID)
 }
 
 func albumsByArtist(name string) ([]Album, error) {
@@ -80,7 +90,7 @@ func albumByID(id int64) (Album, error) {
 	var alb Album
 
 	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
-
+	//
 	//err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price)
 	//if err != nil {
 	//	if err == sql.ErrNoRows {
@@ -97,4 +107,17 @@ func albumByID(id int64) (Album, error) {
 	}
 
 	return alb, nil
+}
+
+func addAlbum(alb Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+
+	return id, nil
 }
