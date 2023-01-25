80bc38570ee65a0b3096ad992ef475ea493ff3d5
Andrew Lundy <andrewlundy@icloud.com>
2023-01-25 07:15:36 -0600

    Move album methods to albums.go

--- main_test.go
+++ main_test.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"testing"
-)
-
 func albumSlicesAreEqual(a, b []Album) bool {
 	if len(a) != len(b) {
 		return false
@@ -17,50 +13,50 @@ func albumSlicesAreEqual(a, b []Album) bool {
 	return true
 }
 
-func TestAlbumsByArtist(t *testing.T) {
-	//name := "John Coltrane"
-
-	wantedResult := make([]Album, 0)
-	wantedResult = append(wantedResult, Album{
-		ID:     1,
-		Title:  "Blue Train",
-		Artist: "John Coltrane",
-		Price:  56.99,
-	})
-
-	albums, err := AlbumsByArtist("John Coltrane")
-	if err != nil {
-		t.Fatalf("Error: %v -- Value of Albums: %v", err, albums)
-	}
-
-	//albums, err := AlbumsByArtist(name)
-	//if err != nil {
-	//	t.Fatalf("Error: %v. Received: %v", err, albums)
-	//}
-	//
-	//t.Errorf("Error: %v, %v", err, albums)
-
-	//gotWantedResult := albumSlicesAreEqual(wantedResult, albums)
-	//fmt.Println(gotWantedResult)
-
-	//wantedResult = append(wantedResult,
-	//	Album{
-	//		ID:     1,
-	//		Title:  "Blue Train",
-	//		Artist: "John Coltrane",
-	//		Price:  56.99,
-	//	},
-	//	Album{
-	//		ID:     2,
-	//		Title:  "Giant Steps",
-	//		Artist: "John Coltrane",
-	//		Price:  63.99,
-	//	})
-	//albums, err := albumsByArtist(name)
-	//
-	//gotWantedResult := albumSlicesAreEqual(wantedResult, albums)
-	//
-	//if !gotWantedResult || err != nil {
-	//	t.Fatalf(`Expected: %v. Received: %v.`, wantedResult, albums)
-	//}
-}
+//func TestAlbumsByArtist(t *testing.T) {
+//	//name := "John Coltrane"
+//
+//	wantedResult := make([]Album, 0)
+//	wantedResult = append(wantedResult, Album{
+//		ID:     1,
+//		Title:  "Blue Train",
+//		Artist: "John Coltrane",
+//		Price:  56.99,
+//	})
+//
+//	albums, err := AlbumsByArtist("John Coltrane")
+//	if err != nil {
+//		t.Fatalf("Error: %v -- Value of Albums: %v", err, albums)
+//	}
+//
+//	//albums, err := AlbumsByArtist(name)
+//	//if err != nil {
+//	//	t.Fatalf("Error: %v. Received: %v", err, albums)
+//	//}
+//	//
+//	//t.Errorf("Error: %v, %v", err, albums)
+//
+//	//gotWantedResult := albumSlicesAreEqual(wantedResult, albums)
+//	//fmt.Println(gotWantedResult)
+//
+//	//wantedResult = append(wantedResult,
+//	//	Album{
+//	//		ID:     1,
+//	//		Title:  "Blue Train",
+//	//		Artist: "John Coltrane",
+//	//		Price:  56.99,
+//	//	},
+//	//	Album{
+//	//		ID:     2,
+//	//		Title:  "Giant Steps",
+//	//		Artist: "John Coltrane",
+//	//		Price:  63.99,
+//	//	})
+//	//albums, err := albumsByArtist(name)
+//	//
+//	//gotWantedResult := albumSlicesAreEqual(wantedResult, albums)
+//	//
+//	//if !gotWantedResult || err != nil {
+//	//	t.Fatalf(`Expected: %v. Received: %v.`, wantedResult, albums)
+//	//}
+//}
